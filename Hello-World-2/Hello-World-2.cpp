e25cbca3d02b7da2e160d3e53a230ee305f41270
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2024-09-10 19:12:10 -0400

    Cloned new version and added a print statement.  Committing

--- Hello-World-2/Hello-World-2.cpp
+++ Hello-World-2/Hello-World-2.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << " Hello again - this time with updated .gitignore\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
